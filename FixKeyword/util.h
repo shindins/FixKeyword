28ce46e1fbff24ddf5a12facb28d36ad410b73b1
신동재 <sindj0267@naver.com>
2025-05-22 13:36:23 +0900

    [feat] mission 1

--- FixKeyword/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef KEYWORD_UTIL_H_
-#define KEYWORD_UTIL_H_
-
-#include <string>
-
-int levenshtein(const std::string& a, const std::string& b);
-bool similer(const std::string& a, const std::string& b);
-
-#endif // !KEYWORD_UTIL_H_
